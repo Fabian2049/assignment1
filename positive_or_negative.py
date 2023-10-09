c3740754cea87d84136e104900a1e0882c5e42d5
Fabian Bernad <fabianbernard49@gmail.com>
2023-10-09 12:04:37 +0100

    i created a code which determines the whether a num is <,> or = to zero

---
+++ positive_or_negative.py
@@ -0,0 +1,9 @@
+import random
+number = random.randint(-15,15)
+print(number)
+if number > 0:
+    print("number is greater than 0")
+if number < 0:
+    print("number is less than 0")
+if number == 0:
+    print("number is zero")
